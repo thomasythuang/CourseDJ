cb88a935761d5d4fcd47cf8d8467d21c8dfcf8f1
thomasythuang <thomas.yt.huang@gmail.com>
2014-09-16 11:23:09 -0500

    init with some basic resources

---
+++ public/js/controllers/mainCtrl.js
@@ -0,0 +1,9 @@
+// js/controllers/mainCtrl.js
+
+// Global Controller- contains functions needed for multiple views
+
+var app = angular.module('mainController', []);
+
+app.controller('mainController', function($scope){
+	
+});
---
+++ public/js/coreModule.js
@@ -0,0 +1,10 @@
+// public/js/coreModule.js
+
+var app = angular.module('CourseDJ', [
+	'ngRoute',
+	'ngRoutes',
+	'coreDirective',
+	'mainController',
+	'mainService',
+	'truncate',
+]);	
