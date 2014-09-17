b6f9f9b144d90ba4d2c606418a37a722e57f6958
thomasythuang <thomas.yt.huang@gmail.com>
2014-09-16 20:13:39 -0500

    get calendar working lolz

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-	scheduler.init('scheduler_here', new Date(),"week");
+    scheduler.config.first_hour = 7;
+    scheduler.config.last_hour = 24;
+    scheduler.config.start_on_monday = false;
+    scheduler.config.dblclick_create = false;
+    scheduler.config.mark_now = true;
+    scheduler.init('scheduler_here', new Date('4/20/2014'), "week"); //Set for 1st week of qtr here
 
-	
 });
