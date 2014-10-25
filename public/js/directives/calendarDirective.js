5c83ba5ff1fe00c563f5f9d025ec5e682187f695
christinakim <christinahkim1@gmail.com>
2014-10-25 15:49:17 -0500

    added color and fixed pick and add class

--- public/js/directives/calendarDirective.js
+++ public/js/directives/calendarDirective.js
@@ -41,7 +41,7 @@ app.directive('dhxScheduler', function() {
 
       // scheduler config
       scheduler.config.first_hour = 7;
-      scheduler.config.last_hour = 24;
+      scheduler.config.last_hour = 22;
       scheduler.config.start_on_monday = false;
       scheduler.config.dblclick_create = false;
       scheduler.config.mark_now = true;
