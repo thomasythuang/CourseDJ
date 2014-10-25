c2e5cb48d7328c3550fb186ed4480817f7048da2
thomasythuang <thomas.yt.huang@gmail.com>
2014-10-25 15:29:36 -0500

    implement remix & scramble

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -9,6 +9,7 @@ app.controller('mainController', function($scope, $http){
 	$scope.term = 4530;
 	$scope.setList = [];
 	$scope.events = [];
+	$scope.courseCount = 4;
 	
 	// GET a list of subjects 
 	$http.get('http://api.asg.northwestern.edu/subjects/?key=a3iSOsJ77pgC8BnX')
@@ -42,6 +43,7 @@ app.controller('mainController', function($scope, $http){
 	$scope.addToSetList = function(course){
 		var i = arrayContains($scope.setList, course);
 		if (i === false){
+			course.priority = 1;
 			$scope.setList.push(course);
 			//console.log($scope.setList);
 		}
@@ -57,31 +59,46 @@ app.controller('mainController', function($scope, $http){
 
 	$scope.remix = function(){
 		$scope.events = [];
-		$scope.chosenDates = $scope.scramble();
-		for (var i=0; i<$scope.setList.length; i++){
-			var title = $scope.setList[i].subject + " " + $scope.setList[i].catalog_num
-			var days = parseDays($scope.setList[i].meeting_days);
+		var chosenDates = scramble($scope.setList);
+		for (var i=0; i<chosenDates.length; i++){
+			var title = chosenDates[i].subject + " " + chosenDates[i].catalog_num
+			var days = parseDays(chosenDates[i].meeting_days);
 			for (var j=0; j<days.length; j++){				
 				$scope.events.push({
 					text: title,
-					start_date: makeDate(days[j], $scope.setList[i].start_time),
-					end_date: makeDate(days[j], $scope.setList[i].end_time),
-				}); 
+					start_date: makeDate(days[j], chosenDates[i].start_time),
+					end_date: makeDate(days[j], chosenDates[i].end_time),
+				});
 			}
 		}
 	}
 
-	$scope.scramble = function(){
+	$scope.test = function(){
 		
+	}
+
+	function scramble(arr){
+		var chosenDates = JSON.parse(JSON.stringify(arr));
+
+		for (var i=0; i<chosenDates.length; i++){
+			if (chosenDates[i].priority < 10){
+				chosenDates[i].priority = chosenDates[i].priority * Math.random();
+			}
+		}
+		chosenDates.sort(function(a, b){return b.priority - a.priority});
+		chosenDates = chosenDates.slice(0, $scope.courseCount);
+		return chosenDates;
 	}
 
 	// take a string "MoWeFr" and returns an array of the dates ["Mo", "We", "Fr"]
   function parseDays(str){
   	var days = [];
-  	var length = str.length;
-  	for (var i=0; i<str.length; i+=2){
-  		days.push(str.substring(i, i+2));
-  	}
+  	if (str != null){
+	  	var length = str.length;
+	  	for (var i=0; i<str.length; i+=2){
+	  		days.push(str.substring(i, i+2));
+	  	}
+	  }
   	return days;
   }
 
